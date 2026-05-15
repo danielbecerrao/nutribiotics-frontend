323d9d4a9728a1d6d775e7aa33f5c351b38c53ee
dbecerratl <daniel.becerra@teclab.com.co>
2026-05-14 22:32:54 -0500

    feat: add patient prescription detail

---
+++ src/app/patient/prescriptions/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { PatientPrescriptionDetailPage as PatientPrescriptionDetailView } from "./patient-prescription-detail-page";
+
+export default function PatientPrescriptionDetailPage() {
+  return <PatientPrescriptionDetailView />;
+}
